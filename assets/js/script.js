ed8b8f2b2da77010d61d9b003df6171ab9057396
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-06-16 12:44:38 -0300

    Criação da parte de infos dos personagem

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,50 @@
 let barraPesquisa = document.querySelector('input[type=number]');
 let buscar = document.querySelector('input[type=submit]');
+let section = document.querySelector('section');
+let containerGeral = document.querySelector('.container-geral');
+let fundo = document.querySelector('.fundo-preto');
 
 
 buscar.addEventListener('click', async () => {
-    let url = `https://rickandmortyapi.com/api/character/${barraPesquisa.value}`;
+    if (barraPesquisa.value.length > 0 && barraPesquisa.value != '0') {
+        let url = `https://rickandmortyapi.com/api/character/${Number(barraPesquisa.value)}`;
 
-    let req = await fetch(url);
-    let json = await req.json();
-    console.log(json);
+        let req = await fetch(url);
+        let json = await req.json();
+
+        mostrarPersonagem(json);
+        console.log(json);
+
+        section.style.display = 'flex';
+        fundo.style.display = 'flex';
+        containerGeral.style.display = 'none';
+        barraLateral.style.display = 'none';
+    } else {
+        alert('Informe um ID para continuar!');
+    }
+    
 
 });
+
+function mostrarPersonagem(json) {
+    let painelPersonagem = document.querySelector('.infos-personagen');
+
+    let nome = painelPersonagem.querySelector('.name-person');
+    let status = painelPersonagem.querySelector('.status-person');
+    let specie = painelPersonagem.querySelector('.specie-person');
+    let genero = painelPersonagem.querySelector('.genero-person');
+    let origen = painelPersonagem.querySelector('.origen-person');
+    let local = painelPersonagem.querySelector('.local-name');
+
+    nome.innerHTML = `Nome: ${json.name}`
+
+}
+
+
+
+
+
+
 
 let barraLateral = document.querySelector('aside');
 let personagem = barraLateral.querySelector('.personagem');
@@ -22,7 +57,6 @@ async function carregarId() {
     let req = await fetch(url);
     let json = await req.json();
 
-    
     criarAside(json.results);
 }
 
@@ -35,5 +69,5 @@ function criarAside(json) {
         console.log(json[i]);
     };
 };
-carregarId()
+
 
